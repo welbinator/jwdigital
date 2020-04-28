74743c7d336e9170d21f41224bdddd612a4df7d8
James <james.welbes@gmail.com>
2020-04-28 18:46:50 -0500

    fixed menu icon on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 function showNav() {
+	const navbars = document.querySelectorAll(".line-container");
 	document.querySelector("nav").classList.toggle("show-nav");
+
+	navbars.forEach((navbar) => {
+		navbar.classList.toggle("hide");
+	});
 }
