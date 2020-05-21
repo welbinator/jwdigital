3e4cccbd5beecbdfbd2fc9597ce2be6ec1573c82
James <james.welbes@gmail.com>
2020-05-21 17:00:30 -0500

    eslint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,111 @@
 function showNav() {
-	const navbars = document.querySelectorAll(".line-container");
-	const menuIcon = document.querySelector(".menu-icon");
-	document.querySelector("nav").classList.toggle("show-nav");
+  //   const navbars = document.querySelectorAll('.line-container');
+  const menuIcon = document.querySelector('.menu-icon');
+  document.querySelector('nav').classList.toggle('show-nav');
 
-	menuIcon.classList.toggle("nav-open");
+  menuIcon.classList.toggle('nav-open');
 }
 
-const navLinks = document.querySelectorAll("nav ul li");
+const navLinks = document.querySelectorAll('nav ul li');
 
 navLinks.forEach((link) => {
-	link.addEventListener("click", showNav);
+  link.addEventListener('click', showNav);
 });
 
 // cards hover
 
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll('.card');
 
 cards.forEach((card) => {
-	card.addEventListener("mouseover", function () {
-		const image = card.childNodes[1];
-		if (image.nodeName == "IMG") {
-			image.style.opacity = ".6";
-		}
-	});
+  card.addEventListener('mouseover', function() {
+    const image = card.childNodes[1];
+    if (image.nodeName == 'IMG') {
+      image.style.opacity = '.6';
+    }
+  });
 
-	card.addEventListener("mouseout", function () {
-		const image = card.childNodes[1];
-		if (image.nodeName == "IMG") {
-			image.style.opacity = "1";
-		}
-	});
+  card.addEventListener('mouseout', function() {
+    const image = card.childNodes[1];
+    if (image.nodeName == 'IMG') {
+      image.style.opacity = '1';
+    }
+  });
 });
 
 // rotate icons on hover
 
-const icons = document.querySelectorAll(".icon");
+const icons = document.querySelectorAll('.icon');
 
 icons.forEach((icon) => {
-	icon.addEventListener("mouseover", function () {
-		icon.classList.add("spin");
-	});
-	icon.addEventListener("mouseout", function () {
-		icon.classList.remove("spin");
-	});
+  icon.addEventListener('mouseover', function() {
+    icon.classList.add('spin');
+  });
+  icon.addEventListener('mouseout', function() {
+    icon.classList.remove('spin');
+  });
 });
 
 // next section button
 
-const home = document.querySelector("#home").getAttribute("id");
-const nextButton = document.querySelector("#next-container");
+const home = document.querySelector('#home').getAttribute('id');
+const nextButton = document.querySelector('#next-container');
 // let currentSection;
 // let currentSectionId;
 let nextSection;
 let nextSectionId;
 
-nextButton.addEventListener("click", jump);
+nextButton.addEventListener('click', jump);
 function jump(s) {
-	slide = parseInt(slide, 10);
-	nextSection = slide + 1;
-	nextSection = document.querySelector(".section" + nextSection);
+  slide = parseInt(slide, 10);
+  nextSection = slide + 1;
+  nextSection = document.querySelector('.section' + nextSection);
 
-	if (slide == sections.length) {
-		window.scrollTo(0, home);
-	} else {
-		nextSectionId = nextSection.getAttribute("id");
-		let nextSlide = document.getElementById(nextSectionId).offsetTop;
+  if (slide == sections.length) {
+    window.scrollTo(0, home);
+  } else {
+    nextSectionId = nextSection.getAttribute('id');
+    const nextSlide = document.getElementById(nextSectionId).offsetTop;
 
-		window.scrollTo(0, nextSlide);
-	}
+    window.scrollTo(0, nextSlide);
+  }
 }
 
 // team section
 
-function onHover() {
-	const devinCard = document.querySelector("#devin");
-	const devinCardDivs = document.querySelectorAll("#devin div");
+// function onHover() {
+//   const devinCard = document.querySelector('#devin');
+//   const devinCardDivs = document.querySelectorAll('#devin div');
 
-	const jamesCard = document.querySelector("#james");
-	const jamesCardDivs = document.querySelectorAll("#james div");
+//   const jamesCard = document.querySelector('#james');
+//   const jamesCardDivs = document.querySelectorAll('#james div');
 
-	devinCard.addEventListener("mouseover", function () {
-		devinCard.classList.add("active");
-		devinCardDivs.forEach((card) => {
-			card.classList.add("active");
-		});
-	});
+//   devinCard.addEventListener('mouseover', function() {
+//     devinCard.classList.add('active');
+//     devinCardDivs.forEach((card) => {
+//       card.classList.add('active');
+//     });
+//   });
 
-	devinCard.addEventListener("mouseout", function () {
-		devinCard.classList.remove("active");
-		devinCardDivs.forEach((card) => {
-			card.classList.remove("active");
-		});
-	});
+//   devinCard.addEventListener('mouseout', function() {
+//     devinCard.classList.remove('active');
+//     devinCardDivs.forEach((card) => {
+//       card.classList.remove('active');
+//     });
+//   });
 
-	jamesCard.addEventListener("mouseover", function () {
-		jamesCard.classList.add("active");
-		jamesCardDivs.forEach((card) => {
-			card.classList.add("active");
-		});
-	});
+//   jamesCard.addEventListener('mouseover', function() {
+//     jamesCard.classList.add('active');
+//     jamesCardDivs.forEach((card) => {
+//       card.classList.add('active');
+//     });
+//   });
 
-	jamesCard.addEventListener("mouseout", function () {
-		jamesCard.classList.remove("active");
-		jamesCardDivs.forEach((card) => {
-			card.classList.remove("active");
-		});
-	});
-}
+//   jamesCard.addEventListener('mouseout', function() {
+//     jamesCard.classList.remove('active');
+//     jamesCardDivs.forEach((card) => {
+//       card.classList.remove('active');
+//     });
+//   });
+// }
 
-onHover();
+// onHover();
